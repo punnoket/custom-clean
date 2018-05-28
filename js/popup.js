bb0152608dd40fb7b20fe9ac2357e0efe0615584
Your Name <mine_wongteeravit@hotmail.com>
2018-05-28 19:26:42 +0700

    popup

---
+++ js/popup.js
@@ -0,0 +1,30 @@
+
+$("#home").click(function () {
+    window.location = 'index.html'
+ });
+ $("#customize").click(function () {
+    window.location = 'customize.html'
+ });
+ $("#speciel").click(function () {
+    window.location = 'speciel.html'
+ });
+ $("#register").click(function () {
+    window.location = 'register.html'
+ });
+ $("#login").click(function () {
+    $(".relative_pop").show();
+    $(".popup_box").show();
+ });
+ $(".relative_pop").click(function () {
+    $(".popup_box").hide();
+    $(".relative_pop").hide();
+ });
+ 
+
+
+ 
+
+$(".img_cancel").click(function () {
+    $("#popup").hide();
+ });
+
